bbe7f65d1e1647f37196f6bd3f33efedf9fd7945
ramesh <romanchaudhary01994@gmail.com>
2023-02-24 12:24:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Airbnbhome } from "./component/Airbnbhome";
+
+function App() {
+  return (
+    <>
+      <Airbnbhome/>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/component/Airbnbhome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './airbnb.css'
+import img1 from '../image/airbnblogo.png'
+
+export const Airbnbhome = () => {
+  return (
+    <div className='div1'>
+      <div className='div2'>
+        <h1>The Evolution of <br />Airbnb's Frontend</h1>
+        <p>@spikebrehm</p>
+        <img src={img1} alt="" />
+      </div>
+    </div>
+  )
+}
+ 
